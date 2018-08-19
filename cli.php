f292f4a5f107cde74d377495193e16c793fe6ff6
dixmod <dixmod@gmail.com>
2018-08-19 23:24:08 +0300

    create

---
+++ cli.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Dixmod\Command\Processing;
+
+$app = new Application("dixmod console", "v1.0.0");
+$app->add(new Processing());
+$app->run();
